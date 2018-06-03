03d4116b4eab097ff5f445dd56d805e356f1dbf9
DhirajMS <dms25121997@gmail.com>
2018-06-04 00:28:37 +0530

    update

--- drivers/misc/mediatek/lcm/rules.mk
+++ drivers/misc/mediatek/lcm/rules.mk
@@ -15,12 +15,25 @@
 # Makefile for misc devices that really don't fit anywhere else.
 #
 LOCAL_DIR := $(GET_LOCAL_DIR)
+# Vanzo:yucheng on: Fri, 14 Oct 2016 18:34:28 +0800
+# added for aosp management to import our variable
+project_name:=$(shell echo $(VANZO_INNER_PROJECT_NAME))
+ifneq ($(strip $(project_name)),)
+-include $(srctree)/../zprojects/$(project_name)/$(project_name).mk
+ccflags-y += -I$(VANZO_PROJECT_HEADERS)
 
-LCM_DEFINES := $(shell echo $(CONFIG_CUSTOM_LK_LCM) | tr a-z A-Z)
+# here use the CUSTOM_LK_LCM to prio the CUSTOM_LK_LCM when zprojects is open
+ifneq ($(strip $(CUSTOM_KERNEL_LCM)),)
+CUSTOM_LK_LCM = $(CUSTOM_KERNEL_LCM)
+endif
+endif
+# End of Vanzo: yucheng
+LCM_DEFINES := $(shell echo $(CUSTOM_LK_LCM) | tr a-z A-Z)
 DEFINES += $(foreach LCM,$(LCM_DEFINES),$(LCM))
 DEFINES += MTK_LCM_PHYSICAL_ROTATION=\"$(MTK_LCM_PHYSICAL_ROTATION)\"
-
-LCM_LISTS := $(subst ",,$(CONFIG_CUSTOM_LK_LCM))
+DEFINES += LCM_WIDTH=\"$(LCM_WIDTH)\"
+DEFINES += LCM_HEIGHT=\"$(LCM_HEIGHT)\"
+LCM_LISTS := $(subst ",,$(CUSTOM_LK_LCM))
 OBJS += $(foreach LCM,$(LCM_LISTS),$(LOCAL_DIR)/$(LCM)/$(addsuffix .o, $(LCM)))
 OBJS += $(LOCAL_DIR)/mt65xx_lcm_list.o \
 		$(LOCAL_DIR)/lcm_common.o \
--- drivers/misc/mediatek/lens/main2/common/ak7371af/AK7371AF.c
+++ drivers/misc/mediatek/lens/main2/common/ak7371af/AK7371AF.c
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2015 MediaTek Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- */
+* Copyright (C) 2016 MediaTek Inc.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+*/
 
 /*
- * AD5820AF voice coil motor driver
+ * AK7371AF voice coil motor driver
  *
  *
  */
@@ -25,7 +25,7 @@
 #include "lens_info.h"
 
 
-#define AF_DRVNAME "AD5820AF_DRV"
+#define AF_DRVNAME "AK7371AF_DRV"
 #define AF_I2C_SLAVE_ADDR        0x18
 
 #define AF_DEBUG
@@ -47,32 +47,41 @@ static unsigned long g_u4TargetPosition;
 static unsigned long g_u4CurrPosition;
 
 
-static int s4AF_ReadReg(unsigned short *a_pu2Result)
+static int s4AF_ReadReg(u8 a_uAddr, u16 *a_pu2Result)
 {
 	int i4RetValue = 0;
-	char pBuff[2];
+	char pBuff;
+	char puSendCmd[1];
+
+	puSendCmd[0] = a_uAddr;
 
 	g_pstAF_I2Cclient->addr = AF_I2C_SLAVE_ADDR;
 
 	g_pstAF_I2Cclient->addr = g_pstAF_I2Cclient->addr >> 1;
 
-	i4RetValue = i2c_master_recv(g_pstAF_I2Cclient, pBuff, 2);
+	i4RetValue = i2c_master_send(g_pstAF_I2Cclient, puSendCmd, 1);
 
 	if (i4RetValue < 0) {
-		LOG_INF("I2C read failed!!\n");
+		LOG_INF("I2C read - send failed!!\n");
 		return -1;
 	}
 
-	*a_pu2Result = (((u16) pBuff[0]) << 4) + (pBuff[1] >> 4);
+	i4RetValue = i2c_master_recv(g_pstAF_I2Cclient, &pBuff, 1);
+
+	if (i4RetValue < 0) {
+		LOG_INF("I2C read - recv failed!!\n");
+		return -1;
+	}
+	*a_pu2Result = pBuff;
 
 	return 0;
 }
 
-static int s4AF_WriteReg(u16 a_u2Data)
+static int s4AF_WriteReg(u16 a_u2Addr, u16 a_u2Data)
 {
 	int i4RetValue = 0;
 
-	char puSendCmd[2] = { (char)(a_u2Data >> 4), (char)((a_u2Data & 0xF) << 4) };
+	char puSendCmd[2] = { (char)a_u2Addr, (char)a_u2Data };
 
 	g_pstAF_I2Cclient->addr = AF_I2C_SLAVE_ADDR;
 
@@ -81,7 +90,7 @@ static int s4AF_WriteReg(u16 a_u2Data)
 	i4RetValue = i2c_master_send(g_pstAF_I2Cclient, puSendCmd, 2);
 
 	if (i4RetValue < 0) {
-		LOG_INF("I2C send failed!!\n");
+		LOG_INF("I2C write failed!!\n");
 		return -1;
 	}
 
@@ -110,6 +119,20 @@ static inline int getAFInfo(__user struct stAF_MotorInfo *pstMotorInfo)
 	return 0;
 }
 
+static inline int setVCMPos(unsigned long a_u4Position)
+{
+	int i4RetValue = 0;
+
+	i4RetValue = s4AF_WriteReg(0x0, (u16) ((a_u4Position >> 2) & 0xff));
+
+	if (i4RetValue < 0)
+		return -1;
+
+	i4RetValue = s4AF_WriteReg(0x1, (u16) ((g_u4TargetPosition & 0x3) << 6));
+
+	return i4RetValue;
+}
+
 static inline int moveAF(unsigned long a_u4Position)
 {
 	int ret = 0;
@@ -120,9 +143,18 @@ static inline int moveAF(unsigned long a_u4Position)
 	}
 
 	if (*g_pAF_Opened == 1) {
-		unsigned short InitPos;
+		unsigned short InitPos, InitPosM, InitPosL, data;
 
-		ret = s4AF_ReadReg(&InitPos);
+		s4AF_ReadReg(0x02, &data);
+
+		LOG_INF("Addr : 0x02 , Data : %x\n", data);
+
+		/* 00:active mode        10:Standby mode    x1:Sleep mode */
+		s4AF_WriteReg(0x02, 0x00);	/* from Standby mode to Active mode */
+		msleep(20);
+		s4AF_ReadReg(0x0, &InitPosM);
+		ret = s4AF_ReadReg(0x1, &InitPosL);
+		InitPos = ((InitPosM & 0xFF) << 2) + ((InitPosL >> 6) & 0x3);
 
 		if (ret == 0) {
 			LOG_INF("Init Pos %6d\n", InitPos);
@@ -151,8 +183,9 @@ static inline int moveAF(unsigned long a_u4Position)
 
 	/* LOG_INF("move [curr] %d [target] %d\n", g_u4CurrPosition, g_u4TargetPosition); */
 
+	/* s4AF_WriteReg(0x02, 0x00); */
 
-	if (s4AF_WriteReg((unsigned short)g_u4TargetPosition) == 0) {
+	if (setVCMPos(g_u4TargetPosition) == 0) {
 		spin_lock(g_pAF_SpinLock);
 		g_u4CurrPosition = (unsigned long)g_u4TargetPosition;
 		spin_unlock(g_pAF_SpinLock);
@@ -181,7 +214,7 @@ static inline int setAFMacro(unsigned long a_u4Position)
 }
 
 /* ////////////////////////////////////////////////////////////// */
-long AD5820AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command, unsigned long a_u4Param)
+long AK7371AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command, unsigned long a_u4Param)
 {
 	long i4RetValue = 0;
 
@@ -216,15 +249,13 @@ long AD5820AF_Ioctl(struct file *a_pstFile, unsigned int a_u4Command, unsigned l
 /* 2.Shut down the device on last close. */
 /* 3.Only called once on last time. */
 /* Q1 : Try release multiple times. */
-int AD5820AF_Release(struct inode *a_pstInode, struct file *a_pstFile)
+int AK7371AF_Release(struct inode *a_pstInode, struct file *a_pstFile)
 {
 	LOG_INF("Start\n");
 
 	if (*g_pAF_Opened == 2) {
 		LOG_INF("Wait\n");
-		s4AF_WriteReg(200);
-		msleep(20);
-		s4AF_WriteReg(100);
+		s4AF_WriteReg(0x02, 0x20);
 		msleep(20);
 	}
 
@@ -241,7 +272,34 @@ int AD5820AF_Release(struct inode *a_pstInode, struct file *a_pstFile)
 	return 0;
 }
 
-int AD5820AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient, spinlock_t *pAF_SpinLock, int *pAF_Opened)
+int AK7371AF_PowerDown(void)
+{
+	LOG_INF("+\n");
+	if (*g_pAF_Opened == 0) {
+		unsigned short data = 0;
+		int cnt = 0;
+
+		while (1) {
+			data = 0;
+
+			s4AF_WriteReg(0x02, 0x20);
+
+			s4AF_ReadReg(0x02, &data);
+
+			LOG_INF("Addr : 0x02 , Data : %x\n", data);
+
+			if (data == 0x20 || cnt == 3)
+				break;
+
+			cnt++;
+		}
+	}
+	LOG_INF("-\n");
+
+	return 0;
+}
+
+int AK7371AF_SetI2Cclient(struct i2c_client *pstAF_I2Cclient, spinlock_t *pAF_SpinLock, int *pAF_Opened)
 {
 	g_pstAF_I2Cclient = pstAF_I2Cclient;
 	g_pAF_SpinLock = pAF_SpinLock;
